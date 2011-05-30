f530c8454c84f1b25a9a2e3565d344fac9dfb2d5
John Clem <johnnyclem@gmail.com>
2011-05-30 12:59:00 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,33 @@
+Enki::Application.routes.draw do
+  namespace 'admin' do
+    resource :session
+
+    resources :posts, :pages do
+      post 'preview', :on => :collection
+    end
+    resources :comments
+    resources :undo_items do
+      post 'undo', :on => :member
+    end
+
+    match 'health(/:action)' => 'health', :action => 'index', :as => :health
+
+    root :to => 'dashboard#show'
+  end
+
+  resources :archives, :only => [:index]
+  resources :pages, :only => [:show]
+
+  constraints :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/ do
+    post ':year/:month/:day/:slug/comments' => 'comments#index'
+    get ':year/:month/:day/:slug/comments/new' => 'comments#new'
+    get ':year/:month/:day/:slug' => 'posts#show'
+  end
+
+  scope :to => 'posts#index' do
+    get 'posts.:format', :as => :formatted_posts
+    get '(:tag)', :as => :posts
+  end
+
+  root :to => 'posts#index'
+end
